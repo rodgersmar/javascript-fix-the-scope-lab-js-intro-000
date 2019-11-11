3a1e08c0ef377773ee8a4e6807a71f1f79778725
toto <ro.marcus@free.fr>
2019-11-11 18:05:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,11 +20,16 @@ function yourAnimal() {
   return animal;
 }
 
-function add2(n) {
+/*function add2(n) {
   return n + two
 
   // Feel free to move things around!
   const two = 2
+}*/
+
+function add2(n) {
+ var two = 2
+  return n + two;
 }
 
 var funkyFunction = function() {
